b1ae14aa0155fa1b57bd3eead0fb3ec60c58a5ee
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-09-24 23:44:32 +0800

    learn transition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,25 @@
 <template>
   <div class="home">
     <v-container fluid>
-      <v-row>
-        <v-col cols="12">
-          <div class="text-center">
-            <ProfileCard />
-          </div>
-        </v-col>
-        <v-col cols="12">
-          <div class="text-center">
-            <v-row align="center" justify="center">
-              <v-col lg="3" cols="12">
-                <v-btn
-                  width="108"
-                  outlined
-                  rounded
-                  color="teal"
-                  dark
-                  to="/About"
-                  >About</v-btn
-                ></v-col
-              >
-              <v-col lg="3" cols="12"
-                ><v-btn
-                  width="108"
-                  outlined
-                  rounded
-                  color="teal"
-                  dark
-                  to="/Works"
-                  >Works</v-btn
-                ></v-col
-              >
-              <v-col lg="3" cols="12">
-                <v-btn
-                  width="108"
-                  outlined
-                  rounded
-                  color="teal"
-                  dark
-                  to="/Contact"
-                  >Contact</v-btn
-                ></v-col
-              >
-            </v-row>
-          </div></v-col
-        >
-      </v-row>
+      <v-slide-y-transition>
+        <h1 v-show="created">Nur Akmal Jalil</h1>
+      </v-slide-y-transition>
     </v-container>
   </div>
 </template>
 
 <script>
-import ProfileCard from "@/components/ProfileCard";
+//import ProfileCard from "@/components/ProfileCard";
 export default {
   name: "Home",
-  components: {
-    ProfileCard,
+  data() {
+    return {
+      created: false,
+    };
   },
-};
+  created() {
+    setTimeout(() => {
+        this.created = true
+      }, 1000);
+  }};
 </script>
