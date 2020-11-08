57e61f0fcda0dd76ab5f296f20e46f0bd3894b40
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-11-08 23:06:53 +0800

    add work page

--- src/components/shared/Nav.vue
+++ src/components/shared/Nav.vue
@@ -5,9 +5,9 @@
         <strong>NUR AKMAL</strong>
       </a>
       <v-spacer></v-spacer>
-      <a href="" class="mx-6 black--text"> <strong>About</strong> </a>
-      <a href="" class="mx-6 black--text"><strong>Work</strong> </a>
-      <a href="" class="mx-6 black--text"> <strong>Contact</strong> </a>
+      <a href="/About" class="mx-6 black--text"> <strong>About</strong> </a>
+      <a href="/Works" class="mx-6 black--text"><strong>Works</strong> </a>
+      <a href="/Contact" class="mx-6 black--text"> <strong>Contact</strong> </a>
     </v-toolbar>
   </div>
 </template>
@@ -15,6 +15,6 @@
 <style scoped>
 a {
   text-decoration: none;
-  color: black;
+ 
 }
 </style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-    <h1>This is works page</h1>
-    <v-btn rounded color="primary" to="/">Home</v-btn>
+    <v-row v-resize="onResize" align="center" justify="center">
+      <v-sheet
+        :width="windowSize.x"
+        :height="windowSize.y"
+        class="teal accent-3 d-flex flex-column"
+      >
+        <v-card flat class="teal accent-3">
+          <v-row align="center" justify="center">
+            <h1>This is works page</h1>
+          </v-row>
+        </v-card>
+
+        <v-card flat class="teal accent-3">
+          <v-row align="center" justify="center">
+            <v-btn rounded color="primary" to="/">Home</v-btn>
+          </v-row>
+        </v-card>
+      </v-sheet>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      windowSize: {
+        x: 0,
+        y: 0,
+      },
+      created: false,
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.created = true;
+    }, 1000);
+  },
+  mounted() {
+    this.onResize();
+  },
+  methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight };
+    },
+  },
+};
+</script>
